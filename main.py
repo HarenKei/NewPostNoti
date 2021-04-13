2845991114c48dcbd15f8c92ed949948cb09d40f
Seungmin Jeong <47844901+HarenKei@users.noreply.github.com>
2021-04-14 00:46:13 +0900

    making Newpost json file

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 import json
-import myblogparser
+import myblogparser as ps
 from collections import OrderedDict
 #import python_twitter
 
 myblog = requests.get("https://heibondk.tistory.com/")
 soup = BeautifulSoup(myblog.content, "html.parser")
+
 
 def NewPost_file() :
     file_data = OrderedDict()
@@ -14,4 +15,10 @@ def NewPost_file() :
     file_data["date"] = soup.select(".date")[0].get_text()
     with open('NewPost.json', 'w', encoding="utf-8") as make_file:
         json.dump(file_data, make_file, ensure_ascii=False, indent="\t")
+
+NewPost_file()
+
+"""def CompNewPost():
+    with open('ExiNewPost.json') as orig_file and  open('NewPost.json')"""
+
 
