6e44ff73c54caa46a80a8d083440ca781152834c
Seungmin Jeong <47844901+HarenKei@users.noreply.github.com>
2021-04-14 00:09:18 +0900

    Functionalization of Json file making

--- myblogparser.py
+++ myblogparser.py
@@ -6,14 +6,19 @@ from collections import OrderedDict
 myblog = requests.get("https://heibondk.tistory.com/")
 soup = BeautifulSoup(myblog.content, "html.parser")
 
-file_data = OrderedDict()
-file_data["title"] = soup.select(".title")[0].get_text()
-file_data["date"] = soup.select(".date")[0].get_text()
+def exiNewPost_file() :
+    file_data = OrderedDict()
+    file_data["title"] = soup.select(".title")[0].get_text()
+    file_data["date"] = soup.select(".date")[0].get_text()
+    with open('ExiNewPost.json', 'w', encoding="utf-8") as make_file:
+        json.dump(file_data, make_file, ensure_ascii=False, indent="\t")
 
-print(json.dumps(file_data, ensure_ascii=False, indent="\t"))
+exiNewPost_file()
 
-with open('ExiNewPost.json', 'w', encoding="utf-8") as make_file:
-    json.dump(file_data, make_file, ensure_ascii=False, indent="\t")
+
+#print(json.dumps(file_data, ensure_ascii=False, indent="\t"))
+
+
 
 
 #OldNewPost = soup.select(".title")[0].get_text()
