ef35a9b2df6e3e624a2970fc9332e31952da311f
HarenKei <seungmin1266@gmail.com>
2021-04-23 18:10:27 +0900

    edit some code

--- telegram_bot.py
+++ telegram_bot.py
@@ -7,9 +7,6 @@ updates = bot.getUpdates()
 global link
 link = "https://heibondk.tistory.com"
 
-for u in updates :
-    print(u.message.text)
-
 def sendNoti(notiTitle, notiDate, notiLink) :
     chat_id = bot.getUpdates()[-1].message.chat.id
     bot.send_message(chat_id=chat_id, text="STARGAZER 블로그에 다음과 같은 새 포스팅이 있습니다. \n " + "\n" + notiTitle +"\n"+ "업로드일 : " + notiDate + "\n" + link + notiLink)
