a857a9e0351ab827017c2ad834041fcb15cd95c6
Seungmin Jeong <seungmin1266@gmail.com>
2021-09-07 23:56:38 +0900

    Update twitter_bot.py : Edtied some var name

--- twitter_bot.py
+++ twitter_bot.py
@@ -7,12 +7,12 @@ global link
 link = "https://heibondk.tistory.com"
 
 def twitting(notiTitle, notiDate, notiLink):
-        apikey = str(tkn[0]).rstrip('\n') #컨슈머키
-        apisecret = str(tkn[1]).rstrip('\n') #컨슈머 시크릿
+        api_key = str(tkn[0]).rstrip('\n') #컨슈머키
+        api_key_secret = str(tkn[1]).rstrip('\n') #컨슈머 시크릿
         access_token = str(tkn[2]).rstrip('\n') #Access
         access_token_secret = str(tkn[3]).rstrip('\n') #Acceess 시크릿
 
-        auth = tweepy.OAuthHandler(apikey, apisecret)
+        auth = tweepy.OAuthHandler(api_key, api_key_secret)
         auth.set_access_token(access_token, access_token_secret)
         api = tweepy.API(auth)
         api.update_status("STARGAZER 블로그에 다음과 같은 새 포스팅이 있습니다. \n " +
